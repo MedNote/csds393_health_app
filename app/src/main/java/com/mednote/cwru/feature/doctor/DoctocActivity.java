be6e261112febd8d4af25a641d77bd314d73d09a
Minchae Jeong <minchaej@gmail.com>
2022-03-25 15:08:25 -0400

    added initial ui

---
+++ app/src/main/java/com/mednote/cwru/feature/doctor/DoctocActivity.java
@@ -0,0 +1,15 @@
+package com.mednote.cwru.feature.doctor;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mednote.cwru.R;
+
+public class DoctocActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor);
+    }
+}
---
+++ app/src/main/java/com/mednote/cwru/feature/user/UserActivity.java
@@ -0,0 +1,15 @@
+package com.mednote.cwru.feature.user;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mednote.cwru.R;
+
+public class UserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
