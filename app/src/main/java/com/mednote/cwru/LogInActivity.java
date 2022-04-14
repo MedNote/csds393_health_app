f59ada195d061fe4bab3e1921dd790bd7901fcdc
Minchae Jeong <minchaej@gmail.com>
2022-04-14 18:34:29 -0400

    added ui

---
+++ app/src/main/java/com/mednote/cwru/LogInActivity.java
@@ -0,0 +1,49 @@
+package com.mednote.cwru;
+
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.widget.Toolbar;
+
+import com.mednote.cwru.base.BaseActivity;
+import com.mednote.cwru.base.PermissionRequestHandler;
+import com.mednote.cwru.util.helpers.ApplicationContextHelper;
+
+public class LogInActivity extends BaseActivity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_login);
+      Toolbar toolbar = findViewById(R.id.main_toolbar);
+      setSupportActionBar(toolbar);
+      ApplicationContextHelper.getInstance().init(getApplicationContext());
+   }
+
+
+
+   @Override
+   public PermissionRequestHandler getPermissionRequestHandler() {
+      return null;
+   }
+
+   @Override
+   public ActivityResultLauncher<String[]> getRequestPermissionsLauncher() {
+      return null;
+   }
+
+   @Override
+   public boolean shouldShowPermissionRationale(String permission) {
+      return false;
+   }
+
+   @Override
+   public void showPermissionRationaleUI(String permission) {
+
+   }
+
+   @Override
+   public void permissionPreviouslyGranted(String permission) {
+
+   }
+}
