9f2601a21a36cb7246b2a75955424c723caf74dc
Minchae Jeong <minchaej@gmail.com>
2022-04-14 19:49:26 -0400

    added apollo

--- app/src/main/java/com/mednote/cwru/LogInActivity.java
+++ app/src/main/java/com/mednote/cwru/LogInActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.widget.Toolbar;
 import com.mednote.cwru.base.BaseActivity;
 import com.mednote.cwru.base.PermissionRequestHandler;
 import com.mednote.cwru.util.helpers.ApplicationContextHelper;
+import com.apollographql.apollo3.ApolloClient;
 
 public class LogInActivity extends BaseActivity {
 
@@ -18,6 +19,11 @@ public class LogInActivity extends BaseActivity {
       Toolbar toolbar = findViewById(R.id.main_toolbar);
       setSupportActionBar(toolbar);
       ApplicationContextHelper.getInstance().init(getApplicationContext());
+
+      ApolloClient.Builder l = new ApolloClient.Builder();
+      ApolloClient c = l.serverUrl("http://ec2-18-233-36-202.compute-1.amazonaws.com:4000/graphql").build();
+   //problema      c.query(new LaunchListQuery()).execute(new Com);
+
    }
 
 
