ce46ef6e39587d82d802e7a4e8556f3efe4b5343
Alex <arambasek21@gmail.com>
2022-04-17 15:45:25 -0400

    Initial code for connecting wallet to app

--- app/src/androidTest/java/com/mednote/cwru/EncryptionTest.java
+++ app/src/androidTest/java/com/mednote/cwru/EncryptionTest.java
@@ -3,9 +3,14 @@ package com.mednote.cwru;
 import android.content.Context;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.apollographql.apollo3.ApolloCall;
+import com.apollographql.apollo3.ApolloClient;
+import com.apollographql.apollo3.api.ApolloResponse;
+import com.apollographql.apollo3.rx2.Rx2Apollo;
 import com.mednote.cwru.ethereum.ContractInteraction;
 import com.mednote.cwru.util.Encryption;
 
@@ -14,7 +19,10 @@ import org.bouncycastle.jce.spec.IESParameterSpec;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
@@ -24,6 +32,7 @@ import java.security.Security;
 import java.security.interfaces.RSAPrivateCrtKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.MGF1ParameterSpec;
+import java.util.Arrays;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
@@ -33,6 +42,11 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.OAEPParameterSpec;
 import javax.crypto.spec.PSource;
 import javax.xml.bind.DatatypeConverter;
+
+import com.mednote.cwru.RecordByUuidQuery.*;
+
+import io.reactivex.Single;
+import io.reactivex.observers.DisposableSingleObserver;
 
 @RunWith(AndroidJUnit4.class)
 public class EncryptionTest {
@@ -44,10 +58,9 @@ public class EncryptionTest {
         //symmetric key encryption
         SecretKey symmKey = Encryption.createSymmetricKey("AES");
         Log.i("Encryption", "Symmetric key: " + DatatypeConverter.printHexBinary(symmKey.getEncoded()));
-        IvParameterSpec ivParameterSpec = Encryption.generateIv();
-        String ciphertext = Encryption.symmetricEncrypt(symmKey, "Hello, world!", "AES/CBC/PKCS5Padding", ivParameterSpec);
-        Log.i("Encryption", "Ciphertext: " + ciphertext);
-        String plaintext = Encryption.symmetricDecrypt(symmKey, ciphertext, "AES/CBC/PKCS5Padding", ivParameterSpec);
+        byte[] ciphertext = Encryption.symmetricEncrypt(symmKey, "Hello, world!", "AES");
+        Log.i("Encryption", "Ciphertext: " + new String(ciphertext));
+        String plaintext = new String(Encryption.symmetricDecrypt(symmKey, ciphertext, "AES"));
         Log.i("Encryption", "Decrypted plaintext: " + plaintext);
     }
 
@@ -62,7 +75,54 @@ public class EncryptionTest {
         Log.i("Encryption", "Private Key q: " + privKey.getPrimeExponentQ());
         Log.i("Encryption", "Private Key exponent: " + privKey.getPrivateExponent());
         OAEPParameterSpec spec = new OAEPParameterSpec("SHA-256", "MGF1", MGF1ParameterSpec.SHA1, PSource.PSpecified.DEFAULT);
-        byte[] temp = Encryption.encrypt(keys[0], "Hello, world!", spec);
-        Log.i("Encryption", "Decrypted plaintext: " + new String(Encryption.decrypt(keys[1], temp, spec)));
+        byte[] temp = Encryption.encrypt(keys[0], "Hello, world!");
+        String temp2 = Arrays.toString(temp);
+        byte[] temp3 = Encryption.bytesFromString(temp2);
+        Log.i("Encryption", "String Ciphertext: " + temp2);
+        Log.i("Encryption", "Ciphertext: " + Arrays.toString(temp3));
+        Log.i("Encryption", "Ciphertext: " + Arrays.toString(temp));
+        Log.i("Encryption", "Decrypted plaintext: " + new String(Encryption.decrypt(keys[1], temp)));
+    }
+
+    @Test
+    public void testRecordEncryption() throws Exception {
+        ApolloClient.Builder l = new ApolloClient.Builder();
+        ApolloClient client = l.serverUrl("http://ec2-18-233-36-202.compute-1.amazonaws.com:4000/graphql").build();
+
+        ApolloCall<Data> queryCall = client.query(new RecordByUuidQuery("dalsdfasjdfsdf"));
+        Single<ApolloResponse<Data>> queryResponse = Rx2Apollo.single(queryCall);
+
+        queryResponse.subscribe(new DisposableSingleObserver<ApolloResponse<Data>>() {
+                                    @Override
+                                    public void onSuccess(@NonNull ApolloResponse<RecordByUuidQuery.Data> dataApolloResponse) {
+                                        Log.d("minnie",dataApolloResponse.data.toString());
+
+                                        SecretKey symmKey = null;
+                                        Record_by_uuid encryptedRecord = null;
+                                        try {
+                                            symmKey = Encryption.createSymmetricKey("AES");
+                                        } catch (NoSuchAlgorithmException e) {
+                                            e.printStackTrace();
+                                        }
+                                        try {
+                                            encryptedRecord = Encryption.encryptRecord(dataApolloResponse.data.record_by_uuid, symmKey, "AES");
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                        Log.i("Encryption", "Encrypted DOB: " + encryptedRecord.dob);
+                                        Log.i("Encryption", "Encrypted UUID: " + encryptedRecord.uuid);
+                                        Log.i("Encryption", "Encrypted Allergies: " + encryptedRecord.allergies.toString());
+                                        Log.i("Encryption", "Encrypted Immunizations: " + encryptedRecord.immunizations.toString());
+                                        Log.i("Encryption", "Encrypted Medications: " + encryptedRecord.medications.toString());
+                                        Log.i("Encryption", "Encrypted Name: " + encryptedRecord.name.toString());
+                                        Log.i("Encryption", "Encrypted Visit Notes: " + encryptedRecord.visit_notes.toString());
+                                    }
+                                    @Override
+                                    public void onError(@NonNull Throwable e) {
+                                        Log.e("minnie",e.getMessage());
+                                    }
+                                }
+        );
+
     }
 }
--- app/src/main/java/com/mednote/cwru/util/Encryption.java
+++ app/src/main/java/com/mednote/cwru/util/Encryption.java
@@ -1,12 +1,22 @@
 package com.mednote.cwru.util;
 
+import android.util.Log;
+
+import com.mednote.cwru.RecordByUuidQuery.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
 import java.security.spec.RSAKeyGenParameterSpec;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.List;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -63,11 +73,13 @@ public class Encryption {
      * 				BadPaddingException,
      * 				UnsupportedEncodingException
      */
-    public static String symmetricEncrypt(Key key, String plaintext, String transformation, IvParameterSpec iv) throws Exception {
+    public static byte[] symmetricEncrypt(Key key, String plaintext, String transformation) throws Exception {
+        if(plaintext == null) {
+            return null;
+        }
         Cipher cipher = Cipher.getInstance(transformation);
-        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-        byte[] encryptedByte = cipher.doFinal(plaintext.getBytes());
-        return Base64.getEncoder().encodeToString(encryptedByte);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(plaintext.getBytes());
     }
 
 
@@ -84,30 +96,126 @@ public class Encryption {
      * 				IllegalBlockSizeException,
      * 				BadPaddingException
      */
-    public static String symmetricDecrypt(Key key, String ciphertext, String transformation, IvParameterSpec iv) throws Exception {
+    public static byte[] symmetricDecrypt(Key key, byte[] ciphertext, String transformation) throws Exception {
+        if(ciphertext == null) {
+            return null;
+        }
         Cipher cipher = Cipher.getInstance(transformation);
-        cipher.init(Cipher.DECRYPT_MODE, key, iv);
-        byte[] decryptedByte = cipher.doFinal(Base64.getDecoder().decode(ciphertext));
-        return new String(decryptedByte);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(ciphertext);
     }
 
-    public static byte[] encrypt(Key key, String plaintext, OAEPParameterSpec spec) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException, InvalidAlgorithmParameterException, NoSuchProviderException {
-        byte[] message = plaintext.getBytes(StandardCharsets.UTF_8);
+    public static byte[] encrypt(Key key, String plaintext) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException, InvalidAlgorithmParameterException, NoSuchProviderException {
+        if(plaintext == null) {
+            return null;
+        }
+        byte[] message = plaintext.getBytes("UTF-8");
         Cipher ecies = Cipher.getInstance("RSA");
-        ecies.init(Cipher.ENCRYPT_MODE, key, spec);
+        ecies.init(Cipher.ENCRYPT_MODE, key);
         return ecies.doFinal(message);
     }
 
-    public static byte[] decrypt(Key key, byte[] ciphertext, OAEPParameterSpec spec) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchPaddingException, NoSuchProviderException {
+    public static byte[] decrypt(Key key, byte[] ciphertext) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchPaddingException, NoSuchProviderException {
+        if(ciphertext == null) {
+            return null;
+        }
         Cipher ecies = Cipher.getInstance("RSA");
-        ecies.init(Cipher.DECRYPT_MODE, key, spec);
+        ecies.init(Cipher.DECRYPT_MODE, key);
         byte[] decrypted = ecies.doFinal(ciphertext);
         return decrypted;
+    }
+
+    public static Record_by_uuid encryptRecord(Record_by_uuid record, Key key, String transformation) throws Exception {
+        String uuid = null;
+        Name name = null;
+        String dob = null;
+        List<String> allergies = new ArrayList<String>();
+        List<String> medications = new ArrayList<String>();
+        List<Immunization> immunizations = new ArrayList<Immunization>();
+        List<Visit_note> visit_notes = new ArrayList<Visit_note>();
+        for (Field f : record.getClass().getDeclaredFields()) {
+            String fieldName = f.getName();
+            switch(fieldName) {
+                case "uuid": {
+                    uuid = Arrays.toString(Encryption.symmetricEncrypt(key, ((String) f.get(record)), transformation));
+                    break;
+                }
+                case "name": {
+                    Name temp = (Name) f.get(record);
+                    String firstName = Arrays.toString(Encryption.symmetricEncrypt(key, temp.first_name, transformation));
+                    String lastName = Arrays.toString(Encryption.symmetricEncrypt(key, temp.last_name, transformation));
+                    name = new Name(firstName, lastName);
+                    break;
+                }
+                case "dob": {
+                    dob = Arrays.toString(Encryption.symmetricEncrypt(key, ((String) f.get(record)), transformation));
+                    break;
+                }
+                case "allergies": {
+                    allergies = new ArrayList<String>();
+                    List<String> temp = (List<String>) f.get(record);
+                    for (String str : temp) {
+                        allergies.add(Arrays.toString(Encryption.symmetricEncrypt(key, str, transformation)));
+                    }
+                    break;
+                }
+                case "medications": {
+                    medications = new ArrayList<String>();
+                    List<String> temp = (List<String>) f.get(record);
+                    for (String str : temp) {
+                        medications.add(Arrays.toString(Encryption.symmetricEncrypt(key, str, transformation)));
+                    }
+                    break;
+                }
+                case "immunizations": {
+                    List<Immunization> temp = (List<Immunization>) f.get(record);
+                    for(Immunization imm : temp) {
+                        immunizations.add(new Immunization(Arrays.toString(Encryption.symmetricEncrypt(key, imm.immunization, transformation)),
+                                Arrays.toString(Encryption.symmetricEncrypt(key, (String) imm.date, transformation))));
+                    }
+                    break;
+                }
+                case "visit_notes": {
+                    List<Visit_note> temp = (List<Visit_note>) f.get(record);
+                    for(Visit_note vis : temp) {
+                        visit_notes.add(new Visit_note(Arrays.toString(Encryption.symmetricEncrypt(key, vis.note, transformation)),
+                                Arrays.toString(Encryption.symmetricEncrypt(key, (String) vis.date, transformation))));
+                    }
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
+        Log.i("Encryption", "UUID: " + uuid);
+        return new Record_by_uuid(uuid, name, dob, allergies, medications, immunizations, visit_notes);
+    }
+
+    public static Record_by_uuid decryptRecord(Record_by_uuid record, Key key, String transformation) {
+        return null;
     }
 
     public static IvParameterSpec generateIv() {
         byte[] iv = new byte[16];
         new SecureRandom().nextBytes(iv);
         return new IvParameterSpec(iv);
+    }
+
+    /**
+     * Parses a String into the byte array that it represents. It does NOT fetch the byte values
+     * of the individual characters. For instance, the string "[6, -108, 112]" will result in a
+     * byte array with first byte 6, not 91 (byte value of '[').
+     *
+     * @param str String representation of a byte array.
+     * @return byte array
+     */
+    public static byte[] bytesFromString(String str) {
+        String[] temp = str.substring(1, str.length() - 1).split(",");
+        byte[] bytes = new byte[temp.length];
+        for (int i = 0, len=bytes.length; i<len; i++) {
+            bytes[i] = Byte.parseByte(temp[i].trim());
+        }
+        return bytes;
     }
 }
