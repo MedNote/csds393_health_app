0eee088cfdb0292ab263fe9eca23e93166404a57
iap20 <iap20@case.edu>
2022-04-30 20:47:04 -0400

    i hate github

--- app/src/androidTest/java/com/mednote/cwru/SignUpTest.java
+++ app/src/androidTest/java/com/mednote/cwru/SignUpTest.java
@@ -1,2 +1,30 @@
-package com.mednote.cwru;public class SignUpTest {
+package com.mednote.cwru;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SignUpTest {
+
+    private SignUpViewModel signUpViewModel = new SignUpViewModel();
+
+    @Test void VMConstruct(){
+        assertEquals("", signUpViewModel.getFname());
+        assertEquals("", signUpViewModel.getLname());
+        assertEquals("", signUpViewModel.getDOB());
+        assertEquals("", signUpViewModel.getPassword());
+    }
+
+    @Test
+    public void testSetters(){
+        signUpViewModel.setFname("First");
+        assertEquals("First", signUpViewModel.getFname());
+        signUpViewModel.setFname("Last");
+        assertEquals("Last", signUpViewModel.getLname());
+        signUpViewModel.setFname("password");
+        assertEquals("password", signUpViewModel.getPassword());
+        signUpViewModel.setFname("01/01/2000");
+        assertEquals("01/01/2000", signUpViewModel.getDOB());
+
+    }
 }
